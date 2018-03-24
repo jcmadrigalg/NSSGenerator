efa96609b3b863a45183e56418fea80cd66703c0
Juan Carlos Madrigal <charlynet.7@gmail.com>
2018-03-23 23:14:13 -0700

    Update to mocha-param 2.0.0, and update the test.js to use the feature

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 /**
  * @author Juan Carlos Madrigal - jcmadrigalg
  */
-const itParam = require('mocha-param').itParam;
+const itParam = require('mocha-param');
 const Assert = require('chai').assert;
 const sut = require('../library/Functions');
 let parameterizedValues = [], result;
@@ -16,7 +16,7 @@ describe('Function', function() {
 			{ parameter: 9, binaryValue: 1, expectedResult: 9  },
 			{ parameter: 9, binaryValue: 2, expectedResult: 18 }
 		];
-		itParam('Number * binary value should return the expected result', parameterizedValues, function(value) {
+		itParam('${value.parameter} * ${value.binaryValue} should return ${value.expectedResult}', parameterizedValues, function(value) {
 			result = sut.Multiplier(value.parameter, value.binaryValue);
 			Assert.equal(result, value.expectedResult, (value.parameter + ' * ' + value.binaryValue + ' must be ' + value.expectedResult));
 		});
@@ -29,7 +29,7 @@ describe('Function', function() {
 			{ parameter: 15, expectedResult: 6 },
 			{ parameter: 18, expectedResult: 9 },
 		];
-		itParam('Number with 2 digits must be converted to 1 digit', parameterizedValues, function(value) {
+		itParam('Number ${value.parameter} must be converted to ${value.expectedResult}', parameterizedValues, function(value) {
 			result = sut.ConvertNumberTo1Digit(value.parameter);
 			Assert.equal(result, value.expectedResult, (value.parameter + ' must be turn to ' + value.expectedResult));
 		});
@@ -42,7 +42,7 @@ describe('Function', function() {
 			{ parameter: 42, expectedResult: 50 },
 			{ parameter: 75, expectedResult: 80 },
 		];
-		itParam('Get the upper decimal number to the parameter value', parameterizedValues, function(value) {
+		itParam('The upper decimal number of ${value.parameter} should be ${value.expectedResult}', parameterizedValues, function(value) {
 			result = sut.NextUpperDecimalNumber(value.parameter);
 			Assert.equal(result, value.expectedResult, ('For ' + value.parameter + ' must be ' + value.expectedResult));
 		});
@@ -53,27 +53,33 @@ describe('Function', function() {
 			{ baseNSS: '1780581644', verifiedDigit: '8', expectedResult: '17805816448' },
 			{ baseNSS: '1424361805', verifiedDigit: '6', expectedResult: '14243618056' },
 		];
-		itParam('Base NSS + Verified Digit should return a complete NSS', parameterizedValues, function(value) {
+		itParam('${value.baseNSS} + ${value.verifiedDigit} should return the complete NSS of ${value.expectedResult}', parameterizedValues, function(value) {
 			result = sut.GenerateNSS(value.baseNSS, value.verifiedDigit);
 			Assert.equal(result, value.expectedResult, (value.baseNSS + ' + ' + value.verifiedDigit + ' must be ' + value.expectedResult));
 		});
 	});
 
 	describe('GenerateRandomNSSBaseNumber', function() {
-		parameterizedValues = [1, 2, 3, 4, 5];
-		itParam('Random number must have length of 10 digits', parameterizedValues, function(totalNumberExecutions) {
-			result = sut.GenerateRandomNSSBaseNumber();
+		parameterizedValues = [
+			{ baseNSS: sut.GenerateRandomNSSBaseNumber() },
+			{ baseNSS: sut.GenerateRandomNSSBaseNumber() },
+			{ baseNSS: sut.GenerateRandomNSSBaseNumber() },
+			{ baseNSS: sut.GenerateRandomNSSBaseNumber() },
+			{ baseNSS: sut.GenerateRandomNSSBaseNumber() }
+		];
+		itParam('Random number ${value.baseNSS} must have length of 10 digits', parameterizedValues, function(value) {
+			result = value.baseNSS;
 			Assert.equal(result.length, 10, ('Random generated number ' + result + ' must have a length of 10 digits'));
 		});
 	});
-
+	
 	describe('QuantityOfRandomNSSToGenerate', function() {
 		parameterizedValues = [
 			{ parameter: 5, expectedResult: 5 },
 			{ parameter: 1000, expectedResult: 1000 },
 			{ parameter: undefined, expectedResult: 1 }
 		];
-		itParam('Should return a number of NSS to be generated', parameterizedValues, function(value) {
+		itParam('${value.parameter} should return ${value.expectedResult} NSS to be generated', parameterizedValues, function(value) {
 			result = sut.QuantityOfRandomNSSToGenerate(value.parameter);
 			Assert.equal(result, value.expectedResult, (value.parameter + ' must be ' + value.expectedResult));
 		});
